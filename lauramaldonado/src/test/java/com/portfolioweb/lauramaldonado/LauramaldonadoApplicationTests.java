2db4dd74ad9dec1494c6d56c7f96fc202e05f928
LauraMaldonado86 <118750070+LauraMaldonado86@users.noreply.github.com>
2023-04-14 15:25:52 -0300

    Dockerfile

---
+++ lauramaldonado/src/test/java/com/portfolioweb/lauramaldonado/LauramaldonadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioweb.lauramaldonado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauramaldonadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
